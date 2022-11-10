557fac36f0bc2b0718244a80ec4231da9c460fb0
bilday08 <114985986+bilday08@users.noreply.github.com>
2022-11-10 15:46:34 +0700

    commit

--- Careers/Jobs/careers/wp-content/themes/ippsum/bold-page-builder/content_elements/bt_bb_card_image/bt_bb_card_image.js
+++
@@ -1,9 +0,0 @@
-(function($) {
-    "use strict";
-    $('.bt_bb_card_image').hover(function() {
-        $(this).find('.bt_bb_card_image_arrow').animate({
-            height: "toggle",
-            opacity: "toggle"
-        }, 300);
-    });
-})(jQuery);
